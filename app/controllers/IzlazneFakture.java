fd37f0aa19496a069880a7d7dafd5a5cb9681366
nikola sofra <nikola sofra>
2017-03-29 00:26:25 +0200

    Novi komit

---
+++ app/controllers/IzlazneFakture.java
@@ -0,0 +1,131 @@
+package controllers;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import com.thoughtworks.xstream.XStream;
+
+import models.IzlaznaFaktura;
+import models.Otpremnica;
+import models.PoslovnaGodina;
+import models.PoslovniPartner;
+
+import models.StavkeFakture;
+import play.data.validation.Error;
+import play.mvc.Controller;
+
+public class IzlazneFakture extends Controller {
+		
+	public static void show(String mode){
+		List<IzlaznaFaktura> izlazneFakture = IzlaznaFaktura.findAll();
+		List<PoslovniPartner> poslovniPartneri = PoslovniPartner.findAll();
+		List<PoslovnaGodina> poslovneGodine = PoslovnaGodina.findAll();
+		List<Otpremnica> otpremnice = Otpremnica.findAll();
+		if(mode == null || mode.equals("")){
+			mode = "edit";
+		}
+		render(izlazneFakture, poslovniPartneri,
+				poslovneGodine, otpremnice, mode);
+	}
+
+	
+	public static void add(int brojFakture, Date datumFakture,
+			Date datumValute, Date datumObracuna,
+			float ukupnoRobe, float ukupanRabat, float ukupanPorez, float iznosFakture,
+			float iznosFaktureOsnovica, String uplataNaRacun, String pozivNaBroj,
+			String statusFakture, long poslovniPartner, long poslovnaGodina,
+			long otpremnica){
+		if(validation.hasErrors()){
+			for(Error error : validation.errors()){
+				System.out.println(error.message());
+			}
+			validation.keep();
+			show("add");
+		}else{
+			IzlaznaFaktura i = new IzlaznaFaktura();
+			i.brojFakture = brojFakture;
+			i.datumFakture = datumFakture;
+			i.datumValute = datumValute;
+			i.datumObracuna = datumObracuna;
+			i.ukupnoRobe = ukupnoRobe;
+			i.ukupanRabat = ukupanRabat;
+			i.ukupanPorez = ukupanPorez;
+			i.iznosFakture = iznosFakture;
+			i.iznosFakture = iznosFaktureOsnovica;
+			i.uplataNaRacun = uplataNaRacun;
+			i.pozivNaBroj = pozivNaBroj;
+			i.statusFakture = statusFakture;
+			i.poslovniPartner = PoslovniPartner.findById(poslovniPartner);
+			i.poslovnaGodina = PoslovnaGodina.findById(poslovnaGodina);
+			i.otpremnica = Otpremnica.findById(otpremnica);
+			i.save();
+			validation.keep();
+			show("add");
+		}
+	}
+	
+	public static void filter(int brojFakture, Date datumFakture,
+			Date datumValute, Date datumObracuna,
+			float ukupnoRobe, float ukupanRabat, float ukupanPorez, float iznosFakture,
+			float iznosFaktureOsnovica, String uplataNaRacun, String pozivNaBroj,
+			String statusFakture, long poslovniPartner, long poslovnaGodina,
+			long otpremnica){
+		List<IzlaznaFaktura> izlazneFakture = IzlaznaFaktura.find(
+				"byBrojFaktureLikeAndDatumFaktureLike", brojFakture, datumFakture).fetch();
+		String mode = "edit";
+		renderTemplate("IzlazneFakture/show.html", izlazneFakture, mode);
+	}
+	
+	public static void edit(int brojFakture, Date datumFakture,
+			Date datumValute, Date datumObracuna,
+			float ukupnoRobe, float ukupanRabat, float ukupanPorez, float iznosFakture,
+			float iznosFaktureOsnovica, String uplataNaRacun, String pozivNaBroj,
+			String statusFakture, long poslovniPartner, long poslovnaGodina,
+			long otpremnica, long id){
+		IzlaznaFaktura i = IzlaznaFaktura.findById(id);
+		i.brojFakture = brojFakture;
+		i.datumFakture = datumFakture;
+		i.datumValute = datumValute;
+		i.datumObracuna = datumObracuna;
+		i.ukupnoRobe = ukupnoRobe;
+		i.ukupanRabat = ukupanRabat;
+		i.ukupanPorez = ukupanPorez;
+		i.iznosFakture = iznosFakture;
+		i.iznosFakture = iznosFaktureOsnovica;
+		i.uplataNaRacun = uplataNaRacun;
+		i.pozivNaBroj = pozivNaBroj;
+		i.statusFakture = statusFakture;
+		i.poslovniPartner = PoslovniPartner.findById(poslovniPartner);
+		i.poslovnaGodina = PoslovnaGodina.findById(poslovnaGodina);
+		i.otpremnica = Otpremnica.findById(otpremnica);
+		i.save();
+		show("");
+	}
+	
+	public static void delete(long id){
+		IzlaznaFaktura i = IzlaznaFaktura.findById(id);
+		for(StavkeFakture x : i.stavkeFakture){
+			x.delete();
+		}
+		i.delete();
+		show("");
+	}
+	
+	public static void export(long id) throws IOException{
+		
+		IzlaznaFaktura faktura = IzlaznaFaktura.findById(id);
+		
+		XStream xStream = new XStream();
+		String xml = xStream.toXML(faktura);
+		
+		String path = "E:\\faktura_"+faktura.brojFakture+".xml";
+		FileWriter fw = new FileWriter(path);
+		BufferedWriter bw = new BufferedWriter(fw);
+		bw.write(xml);
+		bw.close();
+		show("");
+	}
+}
--- app/controllers/StavkeFaktura.java
+++ app/controllers/StavkeFaktura.java
@@ -3,8 +3,10 @@ package controllers;
 import java.util.List;
 
 import models.IzlaznaFaktura;
+import models.Narudzbenica;
 import models.Porez;
 import models.RobaUsluga;
+import models.StavkaNarudzbenice;
 import models.StavkeFakture;
 import play.data.validation.Error;
 import play.data.validation.Required;
@@ -15,15 +17,14 @@ public class StavkeFaktura extends Controller {
 	public static void show (String mode){
 		List<StavkeFakture> stavkeFakture = StavkeFakture.findAll();
 		List<IzlaznaFaktura> izlazneFakture = IzlaznaFaktura.findAll();
-		List<Porez> porezi = Porez.findAll();
 		List<RobaUsluga> robaUsluga = RobaUsluga.findAll();
 		if (mode == null || mode.equals(""))
 			mode = "edit";
-		render(stavkeFakture, izlazneFakture, porezi, robaUsluga, mode);
+		render(stavkeFakture, izlazneFakture, robaUsluga, mode);
 	}
 	
 	public static void add(@Required float kolicina, float cenaPoJediniciMere, float rabat,
-			float osnovica, long izlaznaFaktura, long porez, long robaUsluga)
+		 float pdv, long izlaznaFaktura, long robaUsluga)
 	{
 		if(validation.hasErrors()) {
 			for(Error error : validation.errors()) {
@@ -33,34 +34,73 @@ public class StavkeFaktura extends Controller {
 			show("add");
 		}else {
 			StavkeFakture stavkeFakture = new StavkeFakture();
+			
 			stavkeFakture.kolicina = kolicina;
 			stavkeFakture.cenaPoJediniciMere = cenaPoJediniciMere;
-			stavkeFakture.rabat = rabat;
-			stavkeFakture.osnovica = osnovica;
-			stavkeFakture.izlaznaFaktura = IzlaznaFaktura.findById(izlaznaFaktura);
-			stavkeFakture.porez = Porez.findById(porez);
+			stavkeFakture.rabat = rabat/100;
+			if(rabat == 0){
+				stavkeFakture.osnovica = cenaPoJediniciMere*kolicina;
+			}else{stavkeFakture.osnovica = cenaPoJediniciMere*kolicina-rabat/100;}
+			stavkeFakture.pdv = pdv/100;
+			stavkeFakture.pdvIznos = stavkeFakture.osnovica*stavkeFakture.pdv;
+			stavkeFakture.ukupanIznos = stavkeFakture.osnovica+stavkeFakture.pdvIznos;
 			stavkeFakture.robaUsluga = RobaUsluga.findById(robaUsluga);
+			stavkeFakture.izlaznaFaktura = IzlaznaFaktura.findById(izlaznaFaktura);
 			
+			IzlaznaFaktura faktura = IzlaznaFaktura.findById(izlaznaFaktura);
+			faktura.ukupanPorez+=stavkeFakture.pdvIznos;
+			faktura.ukupnoRobe+=stavkeFakture.kolicina;
+			faktura.ukupanRabat = 0;
+			faktura.iznosFakture+=stavkeFakture.ukupanIznos;
+			faktura.iznosFaktureOsnovica+=stavkeFakture.osnovica;
+			
+			faktura.save();
 			stavkeFakture.save();
+			
 			validation.keep();
 			show("add");
-	}
+		}
 	}
 	
+	public static void filter(float kolicina, float cenaPoJediniciMere, float rabat,
+			long izlaznaFaktura, float pdv, long robaUsluga){
+		List<StavkeFakture> stavkeFakture = StavkeFakture.find("byKolicinaLikeAndCenaPoJediniciMereLikeAnd"
+				+ "RabatLikeAndIzlaznaFakturaLike"
+				+ "AndPdvLikeAndRobaUslugaLike",
+				kolicina, cenaPoJediniciMere, rabat, izlaznaFaktura, pdv, robaUsluga).fetch();
+		String mode = "edit";
+		renderTemplate("StavkeFaktura/show.html", stavkeFakture, mode);
+		
+	}
 	
 	public static void edit(float kolicina, float cenaPoJediniciMere, float rabat,
-			float osnovica, long izlaznaFaktura, long porez, long robaUsluga, long id)
+			float osnovica, long izlaznaFaktura, float pdv, long robaUsluga, long id)
 	{
+		
 		StavkeFakture stavkeFakture = StavkeFakture.findById(id);
+		System.out.println(stavkeFakture.id);
 		stavkeFakture.kolicina = kolicina;
 		stavkeFakture.cenaPoJediniciMere = cenaPoJediniciMere;
-		stavkeFakture.rabat = rabat;
-		stavkeFakture.osnovica = osnovica;
-		stavkeFakture.izlaznaFaktura = IzlaznaFaktura.findById(izlaznaFaktura);
-		stavkeFakture.porez = Porez.findById(porez);
+		stavkeFakture.rabat = rabat/100;
+		if(rabat == 0){
+			stavkeFakture.osnovica = cenaPoJediniciMere*kolicina;
+		}else{stavkeFakture.osnovica = cenaPoJediniciMere*kolicina-rabat/100;}
+		stavkeFakture.pdv = pdv/100;
+		stavkeFakture.pdvIznos = stavkeFakture.osnovica*stavkeFakture.pdv;
+		stavkeFakture.ukupanIznos = stavkeFakture.osnovica+stavkeFakture.pdvIznos;
 		stavkeFakture.robaUsluga = RobaUsluga.findById(robaUsluga);
+		stavkeFakture.izlaznaFaktura = IzlaznaFaktura.findById(izlaznaFaktura);
 		
+		IzlaznaFaktura faktura = IzlaznaFaktura.findById(izlaznaFaktura);
+		faktura.ukupanPorez+=stavkeFakture.pdvIznos;
+		faktura.ukupnoRobe+=stavkeFakture.kolicina;
+		faktura.ukupanRabat = 0;
+		faktura.iznosFakture+=stavkeFakture.ukupanIznos;
+		faktura.iznosFaktureOsnovica+=stavkeFakture.osnovica;
+		
+		faktura.save();
 		stavkeFakture.save();
+
 		show("");
 	}
 	
